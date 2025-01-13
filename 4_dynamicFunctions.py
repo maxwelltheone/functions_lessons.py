64c41a466ff45d7b06d12bbdf923d3147ca6a3da
Luevanos <pluevanos@cps.k12.il.us>
2025-01-13 09:04:47 -0600

    Video notes

--- 4_dynamicFunctions.py
+++ 4_dynamicFunctions.py
@@ -1,11 +1,52 @@
+# def check_3_digits(list1):
+#     for n in list1:
+#         if n in range(100, 1000):
+#             return True
+#         else:
+#             return False
+
+# result = check_3_digits([515, 996, 200])
+# print(result)
+
+
+# def check_3_digits(list1):
+
+#     three_digit_list = []
+    
+#     for n in list1:
+#         if n in range(100, 1000):
+#             three_digit_list.append(n)
+#         else:
+#             return three_digit_list
+
+# result = check_3_digits([555, 99, 600])
+# print(result)
+
+coffee_prices = [('cappuccino', 1.5)
+                ('espresso', 1.2)
+                ('mocha', 1.9)]
+
+
+def most_expensive_coffee(list_of_prices):
+
+    highest_price = 0
+    my_most_expensive_coffee = ''
+
+    for coffee, price in list_of_prices:
+        if price > highest_price:
+            highest_price = price
+            my_most_expensive_coffee = coffee
+        else:
+            pass
+    
+    return (my_most_expensive_coffee, highest_price)
+
+print(most_expensive_coffee(coffee_prices))
+
+
 # Dynamic Functions Practice #1
 # Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
-
 # Don't call the function, you just need to define it.
-
-
-
-
 
 
 # Dynamic Functions Practice #2
@@ -13,8 +54,6 @@
 
 
 
-
-
-
 # Dynamic Functions Practice #3
-# Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
+# Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.  
+
--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,3 +1,35 @@
+# *args = allows you to pass multiple non-key arguements
+# **kwargs = allows you to pass multuople keywored-arguemets
+# * unpacking operator
+# 1. postitional 3. default 3. keyword 4. ARBUTARY
+
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+
+# print(add(1))
+
+# print(add(1, 2, 3, 4, 5))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end=" ")
+
+# display_name("Dr.","Ivo","Eggman","Robotnik")
+
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_address(street="123 NE Fake St.",
+              city = "Montonia",
+              state="Florida",
+              zip="69420")
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
